ffdc87541ac6852e6006cc47d4099d7fb948c7ac
ilyana19 <aries_2152@hotmail.com>
2019-12-16 23:57:45 -0500

    created input box and a few other text components, added particles bg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+      "value": 160,
+      "density": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 10,
+      "random": true,
+      "anim": {
+        "speed": 4,
+        "size_min": 0.3
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "random": true,
+      "speed": 1,
+      "direction": "top",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    },
+    "modes": {
+      "bubble": {
+        "distance": 250,
+        "duration": 2,
+        "size": 0,
+        "opacity": 0
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 4
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm /> */}
+      <Rank />
+      <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
   );
