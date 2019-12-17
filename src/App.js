e803de5b3ef76bba41eec8e83e15f2dfba4a613d
ilyana19 <aries_2152@hotmail.com>
2019-12-16 21:21:52 -0500

    added logo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Logo />
+      {/* <ImageLinkForm /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
