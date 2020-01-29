74b11298c00a0758312cc19118cd0b3c1b5f6817
ilyana19 <aries_2152@hotmail.com>
2020-01-28 20:49:58 -0500

    added signin form with tachyons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
+import Signin from './components/Signin/Signin.js';
 import './App.css';
 
 const app = new Clarifai.App({
@@ -133,6 +134,7 @@ class App extends Component {
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
         <ImageLinkForm 
