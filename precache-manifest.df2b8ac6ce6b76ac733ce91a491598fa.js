e62ba0ecbfcea908ef7c1eb7826b08e6b9feaf0e
ilyana19 <aries_2152@hotmail.com>
2020-02-04 22:56:03 -0500

    Updates

---
+++ precache-manifest.df2b8ac6ce6b76ac733ce91a491598fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d4263c656dcf750731d861ede1d334",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "69a66948a9299dabbf8a",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "07637e9d9b35a6822ca9",
+    "url": "/face-recognition/static/css/main.1d8b2468.chunk.css"
+  },
+  {
+    "revision": "69a66948a9299dabbf8a",
+    "url": "/face-recognition/static/js/2.141c3b60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-recognition/static/js/2.141c3b60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07637e9d9b35a6822ca9",
+    "url": "/face-recognition/static/js/main.d5f97d49.chunk.js"
+  },
+  {
+    "revision": "33d4f797076eaf36a3f5",
+    "url": "/face-recognition/static/js/runtime-main.68a78706.js"
+  }
+]);
